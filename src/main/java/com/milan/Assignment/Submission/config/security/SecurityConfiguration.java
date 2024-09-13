88f6ea1def5455209b1232e7447c2c6bd63a831e
milanvujanic <milanvujanic79@gmial.com>
2024-09-13 19:57:39 +0200

    updated cors settings to allow requests from http://localhost:3000

--- src/main/java/com/milan/Assignment/Submission/config/security/SecurityConfiguration.java
+++ src/main/java/com/milan/Assignment/Submission/config/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,6 +34,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers("/api/auth/**")
